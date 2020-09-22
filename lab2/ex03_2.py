f0b0009f15e4e4cf1a551a318c0d9e0e40c36c80
Vandyshge <vandyshev.e03@mail.ru>
2020-09-23 00:07:33 +0300

    hi

--- lab2/ex03_2.py
+++ lab2/ex03_2.py
@@ -21,13 +21,19 @@ import turtle as t
 def Trance(x0, y0, Vx, Vy, ay, n, dt):
 	t.shape('turtle')
 	t.penup()
+	t.speed(1)
 	x, y = x0, y0
 	Vx, Vy = Vx, Vy
 	for i in range(n):
 		t.goto(x, y)
 		x += Vx * dt
 		dy = Vy * dt + ay * dt ** 2 / 2
-		
 		Vy += ay * dt
+		if y - dy < 0:
+			y = abs(y - dy)
+			Vy = - Vy
+		else:
+			y += dy
+		
 
-Trance(0, 0, 0, 50, -10, 1000, 0.01)
+Trance(0, 0, 1000, 5000, -100, 10000, 0.01)
